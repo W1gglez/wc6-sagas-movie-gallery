a47cab0b6d438f84e248fcf3e4aa809d3b0bde8a
Logan Weigel <snr.wiggels@gmail.com>
2024-06-07 20:57:36 -0500

    Add back button to details page

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import './MovieDetails.css';
@@ -7,21 +7,29 @@ export default function MovieDetails() {
   const params = useParams();
   const movieDetails = useSelector((store) => store.movieDetails);
   const dispatch = useDispatch();
+  const history = useHistory();
 
   useEffect(() => {
+    console.log('Processing FETCH_DETAILS');
     dispatch({ type: 'FETCH_DETAILS', payload: Number(params.id) });
   }, []);
 
   return (
     <main data-testid='movieDetails'>
-      <h1>{movieDetails[0].title}</h1>
+      <button
+        className='backBtn'
+        onClick={() => history.push('/')}
+      >
+        Back
+      </button>
+      <h1>{movieDetails[0]?.title}</h1>
       <section>
         <img
-          src={movieDetails[0].poster}
+          src={movieDetails[0]?.poster}
           alt='Movie Poster'
         />
         <div>
-          <p>{movieDetails[0].description}</p>
+          <p>{movieDetails[0]?.description}</p>
           {/* <p>
             Genres:{' '}
             {movieDetails.map((item, i) =>
@@ -32,7 +40,7 @@ export default function MovieDetails() {
           </p> */}
           <ul>
             Genres:
-            {movieDetails.map((item, i) => (
+            {movieDetails?.map((item, i) => (
               <li key={i}>{item.movie_genres}</li>
             ))}
           </ul>
